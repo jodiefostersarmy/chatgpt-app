705a2ce80d8838fca4f2339f45f1e501fd36611e
Germs <anthony.kwy@gmail.com>
2023-10-08 12:10:19 +1100

    newest checkpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,42 @@
-import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
 
 export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
   return (
     <div className="w-screen min-h-screen bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-red-900 via-red-900 to-red-900">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="flex flex-col items-center texts-center">
+        <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
             <h1 className='mr-3 text-5xl font-semibold'>Chat with a PDF</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
 
-        <div className="flex mt-2">
-          <Button>Go to Chat</Button>
-        </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chat</Button>}
+          </div>
 
+          <p className="max-w-xl mt-1 text-lg text-stone-400">
+            Join the Army
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileupload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
