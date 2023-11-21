ba8b5811424a64ebb6d8475bbc0e31a9478f7eae
jodiefostersarmy <anthony.kwy@gmail.com>
2023-11-22 00:42:55 +1100

    updated the route, up to pinecone

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,14 +1,15 @@
 'use client';
 import { uploadToS3 } from "@/src/lib/s3";
 import { useMutation } from "@tanstack/react-query";
-import { Inbox } from 'lucide-react';
+import { Inbox, Loader2 } from 'lucide-react';
 import React from 'react';
 import { useDropzone } from 'react-dropzone';
 import axios from "axios";
 import toast from "react-hot-toast";
 
 const FileUpload = () => {
-    const {mutate} = useMutation({
+    const [ uploading, setUploading ] = React.useState(false)
+    const { mutate, isLoading } = useMutation({
         mutationFn: async ({
             file_key,
             file_name
@@ -36,15 +37,15 @@ const FileUpload = () => {
                 return;
             }
             try {
+                setUploading(true)
             const data = await uploadToS3(file)
             if (!data?.file_key || !data.file_name) {
                 toast.error("Something went wrong");
                 return;
             }
-            console.log("data", data);
             mutate(data, {
                 onSuccess: (data) => {
-                    console.log(data);
+                    toast.success(data.message)
                 },
                 onError: (err) => {
                     toast.error("Error creating chat");
@@ -52,19 +53,33 @@ const FileUpload = () => {
             })
             } catch (error) {
                 console.log(error);
+            } finally {
+                setUploading(false)
             };
         },
     });
   return (
     <div className='p-2 bg-white rounded-xl'>
-        <div {...getRootProps({
-            className: 'border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col'
-        })}>
+        <div 
+            {...getRootProps({
+                className: 'border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col'
+            })}
+        >
             <input {...getInputProps()} />
-            <>
+            {(uploading || isLoading) ? (
+              <>
+              {/* loading */}
+              <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+              <p className="mt-2 text-sm text-slate-400">
+                Spilling Tea to GPT
+              </p>
+              </>
+            ) : (
+                <>
                 <Inbox className="w-10 h-10 text-blue-500" />
                 <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
             </>
+            )}
         </div>
     </div>
   );
